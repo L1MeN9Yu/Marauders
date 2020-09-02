44e8a9733688e3d30e2285904b99c1a2e298e37e
L1MeN9Yu <baal998.mophisto@gmail.com>
2020-09-02 15:36:12 +0800

    add comment

--- Sources/CMarauders/CMarauders.c
+++ Sources/CMarauders/CMarauders.c
@@ -1,3 +1,6 @@
 //
 // Created by Mengyu Li on 2020/8/31.
 //
+
+//add a source file to avoid compile failure for the target that only contains header files.
+//Awesome feature for swift package manager.
