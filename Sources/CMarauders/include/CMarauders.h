63cda906e0a4e826384aee1ede870aa23df49db5
daniel <dlevi309@gmail.com>
2022-04-23 02:48:11 -0700

    fix for building x86_64 target

--- Sources/CMarauders/include/CMarauders.h
+++ Sources/CMarauders/include/CMarauders.h
@@ -8,7 +8,6 @@
 
 #include "../xnu/bsd/sys/proc_info.h"
 #include "../xnu/bsd/sys/sys_domain.h"
-#include "../xnu/bsd/sys/pipe.h"
 #include "../xnu/bsd/sys/ev.h"
 #include "../xnu/bsd/net/route.h"
 #include "../xpc/xpc_base.h"
@@ -19,6 +18,7 @@
 
 #include "../xpc/xpc_pipe.h"
 #include "../xnu/bsd/net/net_kev.h"
+#include "../xnu/bsd/sys/pipe.h"
 #include "../xnu/bsd/sys/kern_event.h"
 #include "../dyld/src/dyld_process_info_internal.h"
 #include "../xnu/libsyscall/wrappers/libproc/libproc.h"
